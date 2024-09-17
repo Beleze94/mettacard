d7f418d14c2df2c76386d8d90ea5e61b2ecaf5ab
Beleze94 <gustavo.beleze@eclipseworks.com.br>
2024-09-17 18:07:28 -0300

    botoes prontos, necessário ajuste nos svgs

--- src/app/components/button/index.jsx
+++ src/app/components/button/index.jsx
@@ -1,10 +1,27 @@
 "use client";
+import React from "react";
 import styles from "./button.module.sass";
+import Plus from "./svgs/Plus";
+import ChevronRight from "./svgs/ChevronRight";
 
-export default function Button({ buttonText }) {
-	return (
-		<>
-			<a className={`${styles.button}`}>{buttonText}</a>
-		</>
-	);
+export default function Button({ buttonText, buttonVariant, novoContato }) {
+	if (novoContato) {
+		return (
+			<>
+				<a className={`${styles.button} ${styles.novo}`}>
+					<Plus />
+					<p>Novo contato</p>
+					<ChevronRight />
+				</a>
+			</>
+		);
+	} else {
+		return (
+			<>
+				<a className={`${styles.button} ${styles[buttonVariant]}`}>
+					{buttonText}
+				</a>
+			</>
+		);
+	}
 }
