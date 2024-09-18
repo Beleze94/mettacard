dddd2d19b9b8f2858406ea891ecd0bf6862b49bd
Beleze94 <gustavo.beleze@eclipseworks.com.br>
2024-09-18 19:27:43 -0300

    utilização de react-icons/fa

--- src/app/components/button/index.jsx
+++ src/app/components/button/index.jsx
@@ -1,8 +1,8 @@
 "use client";
 import React from "react";
 import styles from "./button.module.css";
-import Plus from "./svgs/Plus";
-import ChevronRight from "./svgs/ChevronRight";
+import { FaChevronRight, FaPlus } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 export default function Button({
 	href,
@@ -18,9 +18,17 @@ export default function Button({
 					href={href}
 					className={`${styles.button} ${styles.novoContato}`}
 				>
-					<Plus />
+					<IconContext.Provider
+						value={{ className: `${styles.icon}` }}
+					>
+						<FaPlus />
+					</IconContext.Provider>
 					<p>Novo contato</p>
-					<ChevronRight />
+					<IconContext.Provider
+						value={{ className: `${styles.icon}` }}
+					>
+						<FaChevronRight />
+					</IconContext.Provider>
 				</a>
 			</>
 		);
