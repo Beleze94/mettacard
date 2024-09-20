bcf2bbca0c806555136ac88faff3a86d57146a8c
Beleze94 <gustavo.beleze@eclipseworks.com.br>
2024-09-20 10:10:47 -0300

    todos status dos inputs criados

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ import Input from "./components/input";
 import { useState } from "react";
 
 export default function Home() {
-	const [state, setState] = useState("default");
+	const state = "default";
 
 	return (
 		<div className={styles.page}>
